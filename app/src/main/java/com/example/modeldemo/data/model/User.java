c65b0f5ef1ddfd9ebd94502254d3809f7e4d8aba
quentin-wang01 <60809302+quentin-wang01@users.noreply.github.com>
2026-06-11 10:04:23 +0800

    添加用户信息实体类

---
+++ app/src/main/java/com/example/modeldemo/data/model/User.java
@@ -0,0 +1,72 @@
+package com.example.modeldemo.data.model;
+
+import com.google.gson.annotations.SerializedName;
+
+/**
+ * 用户信息实体类
+ */
+public class User {
+    @SerializedName("id")
+    private String id;
+
+    @SerializedName("username")
+    private String username;
+
+    @SerializedName("email")
+    private String email;
+
+    @SerializedName("avatar")
+    private String avatar;
+
+    @SerializedName("nickname")
+    private String nickname;
+
+    public User() {
+    }
+
+    public User(String id, String username, String email) {
+        this.id = id;
+        this.username = username;
+        this.email = email;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getAvatar() {
+        return avatar;
+    }
+
+    public void setAvatar(String avatar) {
+        this.avatar = avatar;
+    }
+
+    public String getNickname() {
+        return nickname;
+    }
+
+    public void setNickname(String nickname) {
+        this.nickname = nickname;
+    }
+}
